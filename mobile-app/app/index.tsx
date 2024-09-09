708a4a455eb28cd17d90b1a3a1c216dd8caea5c4
Iarla Crewe <44999892+iarla-crewe@users.noreply.github.com>
2024-09-09 19:28:24 +0100

    Add base Phantom deep-linking scaffold

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -1,15 +1,114 @@
-import { Text, View } from "react-native";
+import "react-native-get-random-values";
+import "react-native-url-polyfill/auto";
+import { Buffer } from "buffer";
+global.Buffer = global.Buffer || Buffer;
+import { StatusBar } from "expo-status-bar";
+import React, { useRef, useState } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Transaction,
+} from "@solana/web3.js";
+import Button from "../components/Button";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { COLORS } from "../constants";
+import * as Linking from "expo-linking";
 
-export default function Index() {
+const connection = new Connection(clusterApiUrl("devnet"));
+
+export default function App() {
+  const [phantomWalletPublicKey, setPhantomWalletPublicKey] =
+    useState<PublicKey | null>(null);
+
+  const [submitting, setSubmitting] = useState(false);
+
+  // Initiate a new connection to Phantom
+  const connect = async () => {};
+
+  // Initiate a disconnect from Phantom
+  const disconnect = async () => {};
+
+  // Initiate a new transaction via Phantom. We call this in `components/AddReviewSheet.tsx` to send our movie review to the Solana network
+  const signAndSendTransaction = async (transaction: Transaction) => {};
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          {phantomWalletPublicKey ? (
+            <>
+              <View style={[styles.row, styles.wallet]}>
+                <View style={styles.greenDot} />
+                <Text
+                  style={styles.text}
+                  numberOfLines={1}
+                  ellipsizeMode="middle"
+                >
+                  {`Connected to: ${phantomWalletPublicKey.toString()}`}
+                </Text>
+              </View>
+              <View style={styles.row}>
+                <Button title="Disconnect" onPress={disconnect} />
+              </View>
+            </>
+          ) : (
+            <View style={{ marginTop: 15 }}>
+              <Button title="Connect Phantom" onPress={connect} />
+            </View>
+          )}
+        </View>
+        {submitting && (
+          <ActivityIndicator
+            color={COLORS.WHITE}
+            size="large"
+            style={styles.spinner}
+          />
+        )}
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.DARK_GREY,
+    flexGrow: 1,
+    position: "relative",
+  },
+  greenDot: {
+    height: 8,
+    width: 8,
+    borderRadius: 10,
+    marginRight: 5,
+    backgroundColor: COLORS.GREEN,
+  },
+  header: {
+    width: "95%",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  spinner: {
+    position: "absolute",
+    alignSelf: "center",
+    top: "50%",
+    zIndex: 1000,
+  },
+  text: {
+    color: COLORS.LIGHT_GREY,
+    width: "100%",
+  },
+  wallet: {
+    alignItems: "center",
+    margin: 10,
+    marginBottom: 15,
+  },
+});
---
+++ mobile-app/constants/index.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const MOVIE_REVIEW_PROGRAM_ID = new PublicKey(
+  "CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN"
+);
+
+export const COLORS = {
+  GREEN: "#21E56F",
+  PURPLE: "#4E44CE",
+  RED: "#EB3742",
+  WHITE: "#FFF",
+  GREY: "rgb(42, 42, 42)",
+  GREY_2: "rgb(91, 91, 91)",
+  DARK_GREY: "rgb(34, 34, 34)",
+  DARKER_GREY_2: "rgb(24, 24, 24)",
+  LIGHT_GREY: "rgb(119, 119, 119)",
+};
