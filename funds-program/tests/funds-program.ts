c39de89e83170fc4cb00a5749a2e074a206349ab
iarla.crewe <iarla.crewe@gmail.com>
2024-09-16 16:57:01 +0100

    Add transfer_lamports tests

--- funds-program/tests/funds-program.ts
+++ funds-program/tests/funds-program.ts
@@ -20,7 +20,7 @@ describe("funds-program", () => {
   const quartzManagerKeypair = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(process.env.QUARTZ_MANAGER_KEYPAIR)));
 
   // Generate random keypairs
-  const randomKeypair = Keypair.generate();
+  const incorrectKeypair = Keypair.generate();
   const userKeypair = Keypair.generate();
 
   const [vaultPda] = anchor.web3.PublicKey.findProgramAddressSync(
@@ -30,14 +30,14 @@ describe("funds-program", () => {
 
   before(async () => {
     console.log("Generated user address: ", userKeypair.publicKey.toString());
-    console.log("Generated random address: ", randomKeypair.publicKey.toString());
+    console.log("Generated incorrect address: ", incorrectKeypair.publicKey.toString());
 
     // Topup user account with SOL
     const tx = new Transaction().add(
       SystemProgram.transfer({
         fromPubkey: wallet.publicKey,
-        toPubkey: randomKeypair.publicKey,
-        lamports: LAMPORTS_PER_SOL * 100,
+        toPubkey: incorrectKeypair.publicKey,
+        lamports: LAMPORTS_PER_SOL * 10,
       })
     );
     await provider.sendAndConfirm(tx);
@@ -55,11 +55,11 @@ describe("funds-program", () => {
         .accounts({
           // @ts-ignore - Causing an issue in Curosr IDE
           vault: vaultPda,
-          quartzManager: randomKeypair.publicKey,
+          quartzManager: incorrectKeypair.publicKey,
           user: userKeypair.publicKey,
           systemProgram: SystemProgram.programId,
         })  
-        .signers([randomKeypair])
+        .signers([incorrectKeypair])
         .rpc();
 
       assert.fail(0, 1, "init_user instruction call should have failed");
@@ -70,11 +70,11 @@ describe("funds-program", () => {
   });
 
 
-  it("init_user self initialize", async () => {
+  it("init_user init by user", async () => {
     const desiredErrorCode = "InvalidQuartzAccount";
 
     const [randomVaultPda] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("vault"), randomKeypair.publicKey.toBuffer()],
+      [Buffer.from("vault"), incorrectKeypair.publicKey.toBuffer()],
       program.programId
     );
 
@@ -84,11 +84,11 @@ describe("funds-program", () => {
         .accounts({
           // @ts-ignore - Causing an issue in Curosr IDE
           vault: randomVaultPda,
-          quartzManager: randomKeypair.publicKey,
-          user: randomKeypair.publicKey,
+          quartzManager: incorrectKeypair.publicKey,
+          user: incorrectKeypair.publicKey,
           systemProgram: SystemProgram.programId,
         })  
-        .signers([randomKeypair])
+        .signers([incorrectKeypair])
         .rpc();
 
       assert.fail(0, 1, "init_user instruction call should have failed");
@@ -117,6 +117,107 @@ describe("funds-program", () => {
   });
 
 
+  it("transfer_lamports", async () => {
+    const destinationAccount = Keypair.generate().publicKey;
+    expect(
+      await provider.connection.getBalance(destinationAccount)
+    ).to.equal(0);
+
+    // Send SOL to vaultPda
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: provider.wallet.publicKey,
+        toPubkey: vaultPda,
+        lamports: LAMPORTS_PER_SOL * 2,
+      })
+    )
+    await provider.sendAndConfirm(transaction);
+
+    // Call PDA to transfer SOL
+    await program.methods
+      .transferLamports(new anchor.BN(LAMPORTS_PER_SOL))
+      .accounts({
+        // @ts-ignore - Causing an issue in Cursor IDE
+        vault: vaultPda, 
+        receiver: destinationAccount,
+        user: userKeypair.publicKey,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([userKeypair])
+      .rpc();
+
+    // Check SOL is received
+    expect(
+      await provider.connection.getBalance(destinationAccount)
+    ).to.equal(LAMPORTS_PER_SOL)
+  })
+
+
+  it("transfer_lamports insufficient funds", async () => {
+    const desiredErrorCode = "InsufficientFunds"
+    const destinationAccount = Keypair.generate().publicKey;
+
+    try {
+      await program.methods
+      .transferLamports(new anchor.BN(LAMPORTS_PER_SOL * 1000))
+        .accounts({
+          // @ts-ignore - Causing an issue in Cursor IDE
+          vault: vaultPda, 
+          receiver: destinationAccount,
+          user: userKeypair.publicKey,
+          systemProgram: SystemProgram.programId
+        })
+        .signers([userKeypair])
+        .rpc();
+
+      assert.fail(0, 1, "transferLamports instruction call should have failed")
+    } catch(err) {
+      expect(err).to.be.instanceOf(AnchorError)
+      expect((err as AnchorError).error.errorCode.code).to.equal(desiredErrorCode)
+    }
+  })
+
+
+  it("transfer_lamports incorrect signature", async () => {
+    const desiredErrorMessage = "Missing signature"
+    const destinationAccount = Keypair.generate().publicKey;
+
+    try {
+      expect(
+        await provider.connection.getBalance(destinationAccount)
+      ).to.equal(0);
+
+      // Send SOL to vaultPda
+      const transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: provider.wallet.publicKey,
+          toPubkey: vaultPda,
+          lamports: LAMPORTS_PER_SOL * 2,
+        })
+      )
+      await provider.sendAndConfirm(transaction);
+
+      // Call PDA to transfer SOL
+      await program.methods
+        .transferLamports(new anchor.BN(LAMPORTS_PER_SOL))
+        .accounts({
+          // @ts-ignore - Causing an issue in Cursor IDE
+          vault: vaultPda, 
+          receiver: destinationAccount,
+          user: userKeypair.publicKey,
+          systemProgram: SystemProgram.programId
+        })
+        .signers([quartzManagerKeypair])
+        .rpc();
+
+      assert.fail(0, 1, "transferLamports instruction call should have failed")
+    } catch(err) {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.include(desiredErrorMessage);
+    }
+  })
+
+
   it("close_user incorrect quartz manager", async () => {
     const desiredErrorCode = "InvalidQuartzAccount";
 
@@ -126,7 +227,7 @@ describe("funds-program", () => {
         .accounts({
           // @ts-ignore - Causing an issue in Curosr IDE
           vault: vaultPda,
-          quartzManager: randomKeypair.publicKey,
+          quartzManager: incorrectKeypair.publicKey,
           user: userKeypair.publicKey,
         })  
         .signers([userKeypair])
@@ -140,7 +241,30 @@ describe("funds-program", () => {
   });
 
 
-  it("close_user incorrect user", async () => {
+  it("close_user close by quartz", async () => {
+    const desiredErrorMessage = "Missing signature"
+
+    try {
+      await program.methods
+        .closeUser()
+        .accounts({
+          // @ts-ignore - Causing an issue in Cursor IDE
+          vault: vaultPda,
+          quartzManager: quartzManagerKeypair.publicKey,
+          user: userKeypair.publicKey
+        })
+        .signers([quartzManagerKeypair])
+        .rpc();
+
+      assert.fail("close_user instruction should have failed");
+    } catch (err) {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.include(desiredErrorMessage);
+    }
+  });
+
+
+  it("close_user close by incorrect user", async () => {
     const desiredErrorMessage = "unknown signer"
 
     try {
@@ -152,7 +276,7 @@ describe("funds-program", () => {
           quartzManager: quartzManagerKeypair.publicKey,
           user: userKeypair.publicKey
         })
-        .signers([randomKeypair])
+        .signers([incorrectKeypair])
         .rpc();
 
       assert.fail("close_user instruction should have failed");
