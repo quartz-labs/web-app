f73bcade9e7395508ac469ed8fed5237b028bf8d
iarla.crewe <iarla.crewe@gmail.com>
2024-09-26 19:29:34 +0100

    Re-init nextjs project

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import React from "react";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { useMemo } from "react";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +16,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+          {children}
       </body>
     </html>
   );
