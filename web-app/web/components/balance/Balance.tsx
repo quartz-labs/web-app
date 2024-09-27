e91660050c9db89538097dfe6e1b0772347485f1
iarla.crewe <iarla.crewe@gmail.com>
2024-09-27 06:38:18 +0100

    Improve balance UI

--- web-app/web/components/balance/Balance.tsx
+++ web-app/web/components/balance/Balance.tsx
@@ -12,6 +12,22 @@ export default function Balance() {
 
     const [detailedView, setDetailedView] = useState(false);
     const [solBalance, setSolBalance] = useState(0);
+    const [solPrice, setSolPrice] = useState(0);
+
+    const updateSolPrice = async () => {
+        try {
+            const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd');
+            const data = await response.json();
+            setSolPrice(data.solana.usd);
+        } catch {
+            console.error("Error: Unable to reach CoinGecko for price data");
+        }
+    }
+
+    useEffect(() => {
+        const interval = setInterval(updateSolPrice, 5000);
+        return () => clearInterval(interval);
+    }, []);
     
     useEffect(() => {
         const updateBalance = async () => {
@@ -33,6 +49,7 @@ export default function Balance() {
                 const accountInfo = await connection.getAccountInfo(vault);
                 if (accountInfo) {
                     setSolBalance(accountInfo.lamports / LAMPORTS_PER_SOL);
+                    updateSolPrice();
                 } else {
                     throw new Error("Account info not found");
                 }
@@ -53,7 +70,11 @@ export default function Balance() {
     else {
         return (
             <div className={styles.balanceWrapper}>
-                {solBalance}
+                <h2 className={styles.heading}>Spendable Balance</h2>
+                <div className={styles.balanceCell}>
+                    <p className={styles.mainBalance}>${(solBalance * solPrice).toLocaleString('en-IE', {minimumFractionDigits: 2, maximumFractionDigits: 2})}</p>
+                    <p className={styles.subBalance}>{solBalance} SOL</p>
+                </div>
             </div>
         )
     }
