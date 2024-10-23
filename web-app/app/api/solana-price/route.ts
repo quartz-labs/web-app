ff6acde8fc409adeef1bdade44f0e5847c1d18d8
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2024-10-23 19:19:14 +0100

    Small fix

--- web-app/app/api/solana-price/route.ts
+++ web-app/app/api/solana-price/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const response = await fetch('https://quartz-server-puoxw.ondigitalocean.app/get-price?asset=solana');
-    const data = await response.json();
+    const { data } = await response.json();
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error: `Unable to fetch Solana price: ${error}` }, { status: 500 });
