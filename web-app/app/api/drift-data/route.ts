639b57308d0972756aa6abe88bef4775a174ca6e
iarla-crewe <iarla.crewe@gmail.com>
2024-11-06 18:02:43 +0000

    Fix localhost in api route

--- web-app/app/api/drift-data/route.ts
+++ web-app/app/api/drift-data/route.ts
@@ -23,8 +23,8 @@ export async function GET(request: Request) {
   }
 
   try {
-    const response = await fetch(`http://localhost:8080/drift-data?address=${address}&marketIndices=${marketIndices}`);
-    // const response = await fetch(`https://quartz-server-puoxw.ondigitalocean.app/drift-data?address=${address}&marketIndices=${marketIndices}`);
+    // const response = await fetch(`http://localhost:8080/drift-data?address=${address}&marketIndices=${marketIndices}`);
+    const response = await fetch(`https://quartz-server-puoxw.ondigitalocean.app/drift-data?address=${address}&marketIndices=${marketIndices}`);
     if (!response.ok) {
       const errorResponse = await response.json();
       throw new Error(errorResponse);
