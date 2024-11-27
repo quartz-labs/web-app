6d25151606cabd9f99a9772a79a28791b686b755
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2024-11-27 12:07:54 +0000

    Add revese proxy for posthog

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false
-      };
-      return config;
-    },
-    env: {
-      NEXT_PUBLIC_REQUIRE_BETA_KEY: process.env.NEXT_PUBLIC_REQUIRE_BETA_KEY,
-    }
-  };
-  
-  export default nextConfig;
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false
+    };
+    return config;
+  },
+  env: {
+    NEXT_PUBLIC_REQUIRE_BETA_KEY: process.env.NEXT_PUBLIC_REQUIRE_BETA_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://eu.i.posthog.com/decide",
+      },
+    ];
+  }
+};
+
+export default nextConfig;
