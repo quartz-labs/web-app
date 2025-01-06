72390ba57bbef62a2d9b36c0a70b0741db9aa647
iarla-crewe <iarla@quartzpay.io>
2025-01-06 17:01:22 +0000

    Remove public RPC from env config

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,6 @@ const envSchema = z.object({
     NEXT_PUBLIC_REQUIRE_BETA_KEY: z.string().transform((str) => str === "true"),
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().url(),
-    NEXT_PUBLIC_RPC_URL: z.string().url(),
     NEXT_PUBLIC_BETA_KEY_COLLECTION: z.string().refine(
         (str) => {
             try {
@@ -26,7 +25,6 @@ const config: Config = envSchema.parse({
     NEXT_PUBLIC_REQUIRE_BETA_KEY: process.env.NEXT_PUBLIC_REQUIRE_BETA_KEY,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    NEXT_PUBLIC_RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
     NEXT_PUBLIC_BETA_KEY_COLLECTION: process.env.NEXT_PUBLIC_BETA_KEY_COLLECTION,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_UNAVAILABLE_TIME: process.env.NEXT_PUBLIC_UNAVAILABLE_TIME
