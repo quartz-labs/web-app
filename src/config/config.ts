a98808b882ac86044b0a949b062aec3aa5bc94b0
iarla-crewe <iarla@quartzpay.io>
2025-01-06 17:07:21 +0000

    Fix env config for real

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,10 +2,9 @@ import { PublicKey } from '@solana/web3.js';
 import { z } from 'zod';
 
 const envSchema = z.object({
-    NEXT_PUBLIC_REQUIRE_BETA_KEY: z.string().transform((str) => str === "true"),
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().url(),
-    NEXT_PUBLIC_RPC_URL: z.string().url(),
+    NEXT_PUBLIC_REQUIRE_BETA_KEY: z.string().transform((str) => str === "true"),
     NEXT_PUBLIC_BETA_KEY_COLLECTION: z.string().refine(
         (str) => {
             try {
@@ -23,11 +22,11 @@ const envSchema = z.object({
 
 type Config = z.infer<typeof envSchema>;
 const config: Config = envSchema.parse({
-    NEXT_PUBLIC_REQUIRE_BETA_KEY: process.env.NEXT_PUBLIC_REQUIRE_BETA_KEY,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    NEXT_PUBLIC_RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
+    NEXT_PUBLIC_REQUIRE_BETA_KEY: process.env.NEXT_PUBLIC_REQUIRE_BETA_KEY,
     NEXT_PUBLIC_BETA_KEY_COLLECTION: process.env.NEXT_PUBLIC_BETA_KEY_COLLECTION,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_UNAVAILABLE_TIME: process.env.NEXT_PUBLIC_UNAVAILABLE_TIME
 });
 
