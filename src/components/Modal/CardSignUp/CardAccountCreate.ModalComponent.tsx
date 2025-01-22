be3aa468def5d671c69770b55fd9b59a2e382c61
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-01-22 18:55:51 +0000

    Fix import error

--- src/components/Modal/CardSignUp/CardAccountCreate.ModalComponent.tsx
+++ src/components/Modal/CardSignUp/CardAccountCreate.ModalComponent.tsx
@@ -1,5 +1,5 @@
 import type { MarketIndex } from "@quartz-labs/sdk/browser";
-import styles from "./Modal.module.css";
+import styles from "../Modal.module.css";
 
 interface InputSectionProps {
     label?: string;
