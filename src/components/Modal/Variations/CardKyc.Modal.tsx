c0ae41e28d966b069bce679df7fdf0ece3dad6d6
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-01-24 22:45:46 +0000

    Fix KYC link disappearing bug

--- src/components/Modal/Variations/CardKyc.Modal.tsx
+++ src/components/Modal/Variations/CardKyc.Modal.tsx
@@ -2,12 +2,19 @@ import { useStore } from "@/src/utils/store";
 import styles from "../Modal.module.css";
 
 export default function CardKycModal() {
-    const { kycLink } = useStore();
+    const { kycLink, cardUserInfo } = useStore();
 
     return (
         <div className={styles.contentWrapper}>
             <h2 className={styles.heading}>Complete KYC Verification here:</h2>
-            <a href={kycLink} target="_blank" rel="noopener noreferrer" className={styles.kycLink}>{kycLink}</a>
+            {kycLink ? (
+                <a href={kycLink} target="_blank" rel="noopener noreferrer" className={styles.kycLink}>{kycLink}</a>
+            ) : (
+                <a href={`${cardUserInfo?.applicationCompletionLink.url}?userId=${cardUserInfo?.applicationCompletionLink.params.userId}`} target="_blank" rel="noopener noreferrer" className={styles.kycLink}>
+                    {`${cardUserInfo?.applicationCompletionLink.url}?userId=${cardUserInfo?.applicationCompletionLink.params.userId}`}
+                </a>
+            )}
+
         </div>
     );
 }
